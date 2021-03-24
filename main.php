47beb19a6d4cb01ff400f6e2e52fa1851adc70de
chapristi <70172942+chapristi@users.noreply.github.com>
2021-03-24 15:10:28 +0100

    Update main.php

--- main.php
+++ main.php
@@ -1,25 +1,18 @@
-  <?php 
+<?php
 use App\github
-?>
-<?php 
-
+  
 try{
-$ClassGithub = new github("repos/chapristi/projet-front-end-mentor");
-$FunctionRequest = $ClassGithub -> request();
+    $ClassGithub = new github("repos/chapristi/projet-front-end-mentor");
+    $FunctionRequest = $ClassGithub -> request();
 
-}
-catch(Exception $e){
+}catch(CurlException $e){
     echo $e -> getMessage();
 }
-function request($request){
-
-        echo $request;
-   
-}
+  
 ?>
            
        
-        <p class="stars-github"><?php try{ echo request($FunctionRequest['stargazers_count']);}catch(Exception $e){echo "";} ?></p>
+<p class="stars-github"><?= request($FunctionRequest['stargazers_count'])?></p>
        
 
     
