822ff7ec1095cb70a0ae4a1cea010ebf12374494
chapristi <70172942+chapristi@users.noreply.github.com>
2021-06-06 21:48:16 +0200

    ✨

--- main.php
+++ main.php
@@ -1,20 +1,10 @@
 <?php
 use App\github
-  
 try{
     $ClassGithub = new github("repos/chapristi/projet-front-end-mentor");
     $FunctionRequest = $ClassGithub -> request();
-
 }catch(CurlException $e){
     echo $e -> getMessage();
 }
-  
-?>
-           
-       
+?>  
 <p class="stars-github"><?= request($FunctionRequest['stargazers_count'])?></p>
-       
-
-    
-
-
